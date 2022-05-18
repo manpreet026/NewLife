a26f353d1b3b81bba93f4dc1aa583db915d6afc3
manpreet026 <88921450+manpreet026@users.noreply.github.com>
2022-05-18 23:38:41 +0530

    new changes done

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "newlife_login";
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
+
+// $base_url = "http://localhost:4444/";
+// $my_email = "YOUR_EMAIL";
+
+// $smtp['host'] = "smtp.gmail.com";
+// $smtp['user'] = "";
+// $smtp['pass'] = "";
+// $smtp['port'] = 465;
+
+?>
